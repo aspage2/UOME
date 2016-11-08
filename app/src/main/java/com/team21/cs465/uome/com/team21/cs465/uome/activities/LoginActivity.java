5abb263ac95d119d185caa2fe5fd8e2b9ead2bf1
Alex <egapxela@gmail.com>
2016-11-08 15:49:37 -0600

    Hello

---
+++ app/src/main/java/com/team21/cs465/uome/com/team21/cs465/uome/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.team21.cs465.uome.com.team21.cs465.uome.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.team21.cs465.uome.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
