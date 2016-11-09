b07052c7cf6d463c28fec5f4577bc656c9e9ff42
Alex <egapxela@gmail.com>
2016-11-09 16:41:08 -0600

    Created Login Screen

--- app/src/main/java/com/team21/cs465/uome/activities/LoginActivity.java
+++ app/src/main/java/com/team21/cs465/uome/activities/LoginActivity.java
@@ -1,5 +1,6 @@
-package com.team21.cs465.uome.com.team21.cs465.uome.activities;
+package com.team21.cs465.uome.activities;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +12,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ActionBar bar = getSupportActionBar();
+        if (bar != null) {
+            bar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+            bar.setCustomView(R.layout.layout_login_actionbar);
+        }
     }
 }
