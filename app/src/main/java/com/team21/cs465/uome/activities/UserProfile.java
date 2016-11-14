114e91ba66b5741640e787ccc1ff4e10de76c4b0
Rohit Saigal <rohitnsaigal@gmail.com>
2016-11-14 15:51:12 -0600

    Rohit

---
+++ app/src/main/java/com/team21/cs465/uome/activities/UserProfile.java
@@ -0,0 +1,24 @@
+package com.team21.cs465.uome.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import com.team21.cs465.uome.R;
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_friend_profile);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+
+    }
+
+
+}
